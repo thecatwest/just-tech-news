c46c93f902e51c4aee74d9751c385f54196e2b22
TheCatWest <catwestover@outlook.com>
2021-06-07 14:26:07 -0700

    create api routes for post model

--- models/index.js
+++ models/index.js
@@ -3,5 +3,16 @@ const User = require('./User');
 // import Post model
 const Post = require('./Post');
 
+// define model associations
+// create association demonstrating a user has many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+}); 
+
+// create reverse association demonstrating a post belongs to only one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 // export User, Post objects with is as a property
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(routes);
 
 // turn on connection to db and server using .sync() (sync means Sequelize is taking models and connecting them to associated db tables)
 // use of {force: false} prevents dropping and recreating of database tables on startup (code works either way)
+// use of {force: true} forces db connection to sync with model definitions and associations, forcing the tables to re-create if any associations change
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
